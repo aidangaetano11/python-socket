9533927b01d2388ad140157e6a4e3e704e202b23
aidangaetano11 <aidan.gaetano@wsu.edu>
2024-10-18 12:18:27 -0700

    Updated server to send messages to the client

--- client.py
+++ client.py
@@ -4,4 +4,4 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('192.168.0.200', 9999))
 
 client.send("Hello From Client".encode())
-print(client.recv(1024))
+print(client.recv(1024).decode())
--- server.py
+++ server.py
@@ -1,4 +1,20 @@
 import socket
+import threading
+
+def handle_client(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode()
+            if not message:
+                break
+            print(f"Client: {message}")
+
+            reply = input("Server: ")
+            client_socket.send(reply.encode())
+        except:
+            break
+    
+    client_socket.close()
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('0.0.0.0', 9999))
@@ -6,6 +22,7 @@ server.bind(('0.0.0.0', 9999))
 server.listen(5)
 
 while True:
-    client, addr = server.accept()
-    print(client.recv(1024).decode())
-    client.send("Hello From Server".encode())
+    client, address = server.accept()
+    print(f"Received connection from {address}")
+    client_handler = threading.Thread(target=handle_client, args=(client,))
+    client_handler.start()
