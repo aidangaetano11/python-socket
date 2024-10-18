705096b3257298ab5ae870da762ef7f745449742
aidangaetano11 <aidan.gaetano@wsu.edu>
2024-10-18 12:38:37 -0700

    Implemented client receive and send messages.

--- client.py
+++ client.py
@@ -1,7 +1,32 @@
 import socket
+from sys import stdout
+import threading
+
+def send_messages(client_socket):
+    while True:
+        message = input("Client: ")
+        client_socket.send(message.encode())
+
+def receive_messages(client_socket):
+    while True:
+        try:
+            reply = client_socket.recv(1024).decode()
+            if not reply:
+                break
+            stdout.write(f"\nServer: {reply}\nClient: ")
+        except:
+            break
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('192.168.0.200', 9999))
 
-client.send("Hello From Client".encode())
-print(client.recv(1024).decode())
+send_thread = threading.Thread(target=send_messages, args=(client,))
+receive_thread = threading.Thread(target=receive_messages, args=(client,))
+
+receive_thread.start()
+send_thread.start()
+
+receive_thread.join()
+send_thread.join()
+
+client.close()
